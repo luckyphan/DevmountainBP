fab70b493465546c4fa00a023b7b113cfd4c11a7
Lucky Phan <lpt.lucky@gmail.com>
2023-05-04 10:26:36 -0400

    thur codeChallenge guess object gift

---
+++ week_5/thurs/codeChallenge/guessGift.js
@@ -0,0 +1,28 @@
+function guessGifts (wl, pres){
+    let presents = []
+    for(let i = 0; i < pres.length; i++){
+        wl.forEach(element => {
+           if( pres[i].size === element.size){
+                if( pres[i].clatters === element.clatters){
+                    if( pres[i].weight === element.weight){
+                        presents.push(element.name)
+                }
+           }
+        }
+        });
+    }
+    console.log(presents)
+}
+
+var wishlist = [
+    {name: "Mini Puzzle", size: "small", clatters: "yes", weight: "light"},
+    {name: "Toy Car", size: "medium", clatters: "a bit", weight: "medium"},
+    {name: "Card Game", size: "small", clatters: "no", weight: "light"}
+];
+
+var presents = [
+    {size: "medium", clatters: "a bit", weight: "medium"},
+    {size: "small", clatters: "yes", weight: "light"}
+];
+
+guessGifts(wishlist, presents); // must return ["Toy Car", "Mini Puzzle"]
